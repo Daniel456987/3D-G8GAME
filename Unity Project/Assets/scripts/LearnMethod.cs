b729cfd6eae67b93c21e212ad96a095a50705f84
Daniel456987 <55180590+Daniel456987@users.noreply.github.com>
2019-11-20 15:55:10 +0800

    程式

---
+++ Unity Project/Assets/scripts/LearnMethod.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class LearnMethod : MonoBehaviour
+{
+    private void Drive(int speed)
+    {
+        print("開車，時速：200");
+        print("音效~");
+    }
+
+    public void Shoot(int count, float speed, string prop = "無",string direction = "前方")
+    {
+        print("弓箭數量：" + count);
+        print("弓箭速度：" + speed);
+        print("弓箭屬性：" + prop);
+        print("弓箭方向：" + direction);
+    }
+
+    private void Start()
+    {
+        print("哈囉，沃德");
+
+        Drive(200);
+        Drive(300);
+        Drive(400);
+
+        Shoot(1, 1.5f);
+        Shoot(10, 10.5f, "火屬性");
+        Shoot(3, 2, direction: "前後方");
+    }
+}
--- Unity Project/Assets/scripts/chicken.cs
+++ Unity Project/Assets/scripts/chicken.cs
@@ -2,9 +2,13 @@
 
 public class chicken : MonoBehaviour
 {
-    int speed = 10;
-    float turn = 20.5f;
-    bool mission;
-    string name = "G8雞";
-    int 雞蛋數量 = 5;
+    [Header("速度")][Range(1,100)]
+    public int speed = 10;
+    [Tooltip("G8雞旋轉速度")][Range(1,100)]
+    public float turn = 20.5f;
+    public bool mission;
+    public string name = "G8雞";
+    public int 雞蛋數量 = 5;
+
+
 }
