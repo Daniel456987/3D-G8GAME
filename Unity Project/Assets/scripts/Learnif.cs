cc72dea18206fca6a04186c675014f2f53043f37
Daniel456987 <55180590+Daniel456987@users.noreply.github.com>
2019-12-04 16:01:27 +0800

    數學運算 判斷式

---
+++ Unity Project/Assets/scripts/Learnif.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Learnif : MonoBehaviour
+{
+    public bool openDoor;
+
+    public int score;
+
+    [Header("連擊次數"),Range(1,200)]
+    public int combo;
+    private void Start()
+    {
+        if (true)
+        {
+            print("測試");
+        }
+    }
+
+    private void Update()
+    {
+        if (openDoor)
+        {
+            print("開門");
+        }
+        else
+        {
+            print("關門");
+        }
+
+        if (score >= 60)
+        {
+            print("及格");
+        }
+        else if (score >= 50)
+        {
+            print("可以補考");
+        }
+        else if (score >=40)
+        {
+            print("可以付錢補考");
+        }
+
+        if (combo >= 150)
+        {
+            print("攻擊10倍");
+        }
+        else if (combo >= 100)
+        {
+            print("攻擊5倍");
+        }
+        else if (combo >= 50)
+        {
+            print("攻擊2倍");
+        }
+    }
+}
